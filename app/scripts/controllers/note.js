9fe3280831e66650740cb8895e739c81a9d62f49
alfmags <alfmags@gmail.com>
2015-07-19 16:16:04 +0200

    note controller

---
+++ app/scripts/controllers/note.js
@@ -0,0 +1,18 @@
+
+angular
+	.module{'noteController'}.controller{'NoteCtrl', function($scope){
+		
+		this.note = {
+			"Id": id,
+			"Content": content,
+			"Color": color,
+			"Author": author,
+			"Position": Position;
+		}
+
+		this.notes = [];
+
+
+
+	}}
+
