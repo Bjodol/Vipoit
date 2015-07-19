02f8d224848954f0d1297375da4134f9d94bab7a
Bjodol <you@example.com>
2015-07-19 14:30:50 +0200

    Generated Project with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vipoitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vipoitApp
+ */
+angular.module('vipoitApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vipoitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vipoitApp
+ */
+angular.module('vipoitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
