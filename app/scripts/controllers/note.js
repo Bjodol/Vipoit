58b2ef16e1c7a31caf11088954543bd1ef3f82f5
Bjodol <you@example.com>
2015-07-20 15:28:57 +0200

    Added color support

--- app/scripts/controllers/note.js
+++ app/scripts/controllers/note.js
@@ -1,22 +1,45 @@
 
 angular.module('note', ['blackboard'])
 	.controller('noteCtrl', function($scope, blackboardService){
-
-		$scope.addNote = function(note){
-			var newNote = angular.copy(note);
-			
-			blackboardService.notes.push(newNote);
 		
+		$scope.note = {};
+		$scope.note.Color="note-yellow"
+		$scope.colors = blackboardService.noteColors;
+
+		$scope.addNote = function(){
+			var newNote = angular.copy($scope.note);
+			blackboardService.notes.push(newNote);
 		}
 
-	
-
-
-
-		
-
-
-
-
+		$scope.setColor = function(color){
+			switch(color) {
+	      		case 'note-pink':
+	         		$scope.note.Color = "note-pink";
+	         		console.log($scope.note.Color);
+	         	break;
+	      		case 'note-yellow':
+			        $scope.note.Color = "note-yellow";
+			        console.log($scope.note.Color);
+			    break;
+			    case 'note-purple':
+	         		$scope.note.Color = "note-purple";
+	         		console.log($scope.note.Color);
+	         	break;
+	         	case 'note-dark-blue':
+	         		$scope.note.Color = "note-dark-blue";
+	         		console.log($scope.note.Color);
+	         	break;
+	         	case 'note-light-blue':
+	         		$scope.note.Color = "note-light-blue";
+	         		console.log($scope.note.Color);
+	         	break;
+			    case 'note-green':
+	         		$scope.note.Color = "note-green";
+	         		console.log($scope.note.Color);
+	         	break;
+      		default:
+         		console.log('There should not be a default failover at this thing.');
+			}
+		}
 	})
 
